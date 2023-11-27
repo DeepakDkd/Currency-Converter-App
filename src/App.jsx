6d89fe2e1a339c435fdb5b9bee375104849731a8
Deepak Dhakad <deepakdkd1188@gmail.com>
2023-11-28 02:23:43 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import InputBox from './components/InputBox'
-import useCurrencyInfo from './hooks/usecurrencyinfo'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 import './App.css'
 
 function App() {
